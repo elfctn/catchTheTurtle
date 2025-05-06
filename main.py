2f782f8f4ea0652a8180e121e4d0c2df2b16ee13
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-06 14:24:25 +0300

    created make turtle func

--- main.py
+++ main.py
@@ -26,3 +26,22 @@ def setup_score_turtle():
     y = top_height * 0.9
     score_turtle.setposition(0, y)
     score_turtle.write(arg='score:0', move=False, align='center', font=FONT)
+
+
+def make_turtle(x, y):
+    t = turtle.Turtle()
+
+    def handle_click():
+        global score
+        score+=1
+        score_turtle.clear()
+        score_turtle.write(arg='score:  {}'.format(score), move=False, align='center', font=FONT)
+
+
+    t.onclick(handle_click)
+    t.penup()
+    t.shape('turtle')
+    t.shapesize(2,2)
+    t.color('green')
+    t.goto(x * grid_size,y * grid_size)
+    turtle_list.append(t)

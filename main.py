0400464145bbdc019f6f619ac3592facc8ddf008
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-06 14:23:01 +0300

    created setup score func

--- main.py
+++ main.py
@@ -15,3 +15,14 @@ score_turtle = turtle.Turtle()
 countdown_turtle = turtle.Turtle
 
 grid_size = 10
+
+
+def setup_score_turtle():
+    score_turtle.hideturtle()
+    score_turtle.color('dark blue')
+    score_turtle.penup()
+
+    top_height = screen.window_height() / 2
+    y = top_height * 0.9
+    score_turtle.setposition(0, y)
+    score_turtle.write(arg='score:0', move=False, align='center', font=FONT)
